6112250e561101259ed14d111ccde5914863e62f
anjaaviani <anja.aviani@gmail.com>
2022-02-04 20:55:29 +0100

    Python Data Science - practices 4 and 5

---
+++ Practice_4/practice_4.py
@@ -0,0 +1,17 @@
+#Operator +
+print(43+60+16+41)
+
+#Operator -
+print(50-60)
+
+#Operator *
+print(5*5)
+
+#Operator / daje float
+print(25/6)
+
+#Operator // daje int
+print(25//6)
+
+#Excercise
+print(160/60)
---
+++ Practice_5/practice_5.py
@@ -0,0 +1,24 @@
+#Adding the variables
+x=43+16+60+41
+print(x)
+
+#Adding another variable to equation
+y=x/60
+print(y)
+
+#Name variables meaningfully
+total_min=43+50+57
+print(total_min)
+
+total_hours=total_min/60
+print(total_hours)
+
+#Excercise
+a=3+2*2
+print(a)
+
+b=(3+2)*2
+print(b)
+
+c=a+b
+print(c)
